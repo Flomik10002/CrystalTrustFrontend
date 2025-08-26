07e9c59a40826848c02d28f98bf287b18670f533
Flomik <79540285+Flomik10002@users.noreply.github.com>
2025-08-26 04:06:12 +0400

    Fixed: #11 and #12

--- src/app/pages/transfer/internal-transfer-success/internal-transfer-success.ts
+++ src/app/pages/transfer/internal-transfer-success/internal-transfer-success.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {HeaderComponent} from '../../../shared/header.component/header.component';
 import {StoryCard} from '../../../components/story-card/story-card';
+import {Crystal} from '../../../api/crystal';
+import {finalize} from 'rxjs';
 
 @Component({
   selector: 'app-internal-transfer-success',
-  imports: [
-    RouterLink,
-    HeaderComponent,
-    StoryCard
-  ],
+  standalone: true,
+  imports: [RouterLink, HeaderComponent, StoryCard],
   templateUrl: './internal-transfer-success.html',
   styleUrl: './internal-transfer-success.css'
 })
-export class InternalTransferSuccess {
+export class InternalTransferSuccess implements OnInit {
+  nickname = '';
+  avatarUrl = '';
+  loading = true;
 
+  constructor(private api: Crystal) {}
+
+  ngOnInit(): void {
+    this.api.getHomeData()
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe({
+        next: ({ profile }) => {
+          this.nickname = profile?.nickname ?? '';
+          this.avatarUrl = this.nickname
+            ? `https://mc-heads.net/avatar/${this.nickname}`
+            : 'assets/fallback-avatar.png';
+        },
+        error: (err) => {
+          console.error('Profile load failed', err);
+          this.nickname = '';
+          this.avatarUrl = 'assets/fallback-avatar.png';
+        },
+      });
+  }
 }
